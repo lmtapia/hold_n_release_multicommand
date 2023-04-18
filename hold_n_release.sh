0cea6cf0da0d00a95d9e8e053453b025018d51a8
TapiaX <luistapia2998@gmail.com>
2023-04-18 16:31:41 -0400

    delay optional

--- hold_n_release.sh
+++ hold_n_release.sh
@@ -8,18 +8,20 @@
 
 BASE="`dirname "$(readlink -f "$0")"`"
 BUTTON=$1
-DELAY=$2
 PROGS=$BASE/hold_n_release.$BUTTON.commands
 LOCK=$BASE/tmp/hold_n_release.$BUTTON.lock
 LINES=$(wc -l < $PROGS)
-if [ -z "$BUTTON" -o -z "$DELAY" -o ! -f $PROGS ]; then
-  echo "Usage : hold_n_release <Button> <Delay (sec)> [release]"
-  echo "put the commands on a file named $BASE/hold_n_release.<Button>.commands one per line" 
+
+if [ -z "$BUTTON" -o ! -f $PROGS ]; then
+  echo "Usage, for button pressed: hold_n_release <Button>"
+  echo "for release:               hold_n_release <Button> release [delay=1]"
+  echo "Put the commands on a file $BASE/hold_n_release.<Button>.commands one per line"
   exit
 fi
 EPOCH=$(date +'%s')
 
-if [[ "$3" == "release" &&  -e $LOCK ]] ; then
+if [[ "$2" == "release" &&  -e $LOCK ]] ; then
+  if [[ -z "$3" ]]; then DELAY=1; else DELAY=$3; fi
   LASTTIME=`cat $LOCK`
   CHOICE=$(( (EPOCH - LASTTIME) / DELAY + 1 )) 
   # echo $CHOICE
